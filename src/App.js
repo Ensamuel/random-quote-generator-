81d9ab8be5778cdf74fb525c36e142554c28c8f5
ensamuel <ensamuel11@gmail.com>
2022-08-12 01:51:03 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React from "react";
+import { useState, useEffect } from 'react';
 
-function App() {
+
+
+function App(){
+const[quotes, setQuotes] = useState('')
+
+useEffect(()=>{fetchQuotes()},[])
+  const fetchQuotes = async()=>{
+    const {data} = await
+    axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    let quotess = data.quotes
+    let randomNumber = Math.floor(Math.random()*quotess.length)
+    setQuotes(data.quotes[randomNumber])
+  
+}
+const handleClick= ()=>{
+  fetchQuotes()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="quote-box" style={{
+        backgroundColor:'pink',
+        height: '50vh',
+        width: '100%',
+        marginTop: '50px',
+        fontFamily: 'sans-serif'
+      }
+      }>
+
+      <div id="text" style={{
+        
+        fontSize: '30px'
+      }}>
+      {quotes.quote}
+      </div>
+
+      <div id="author" style={
+        {
+          textAlign: 'right',
+          padding: '30px'
+        }
+      }>
+      {quotes.author}
+      </div>
+
+      <button id="new-quote" onClick={handleClick} style={{
+        border: 'none',
+        backgroundColor: 'white',
+        fontSize:'20px',
+        cursor:'pointer'
+      }}> Get Quote
+      </button>
+
+      <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">Tweet
+      </a>
+
     </div>
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
